d600984f09b84acf4ee496d7e813ffba59eeff07
Dooyeon Kang <dyhouse0517@gmail.com>
2020-12-29 15:22:30 +0900

    [기획전 관리] - 서비스 다큐멘테이션 추가 - 모델 다큐멘테이션 추가 - 뷰 다큐멘테이션 추가

--- view/admin/event_view.py
+++ view/admin/event_view.py
@@ -13,6 +13,18 @@ from flask_request_validator import (
 
 
 class EventView(MethodView):
+    """ Presentation Layer
+
+        Attributes:
+            database: app.config['DB']에 담겨있는 정보(데이터베이스 관련 정보)
+            service : EventService 클래스
+
+        Author: 강두연
+
+        History:
+            2020-12-28(강두연): 초기 생성
+            2020-12-29(강두연): 예외 처리 및 검색조건 받기 추가
+    """
 
     def __init__(self, service, database):
         self.service = service
@@ -29,6 +41,62 @@ class EventView(MethodView):
         Param('end_date', JSON, str, required=False, rules=[DateRule()])
     )
     def get(self, *args):
+        """ GET 메소드: 검색 조건에 맞는 기획전 정보를 조회.
+
+            [이름 또는 번호], [기획전 진행상태], [노출여부], [등록일], [페이지 및 갯수] 에 알맞는 기획전을 조회한다
+
+            Args: args = ('name', 'number', 'status', 'exposure', 'page', 'length', 'start_date', 'end_date')
+
+            Author: 강두연
+
+            Returns:
+                return {
+                    "message": "success",
+                    "result": [
+                        {
+                            "created_at": "Mon, 28 Dec 2020 16:40:41 GMT",
+                            "end_date": "Mon, 01 Mar 2021 00:00:00 GMT",
+                            "event_kind": "버튼",
+                            "event_name": "성보의 하루 시리즈2(버튼형)",
+                            "event_number": 2,
+                            "event_status": "진행중",
+                            "event_type": "상품(이미지)",
+                            "is_display": "노출",
+                            "product_count": 59,
+                            "start_date": "Mon, 19 Oct 2020 00:00:00 GMT"
+                        },
+                        {
+                            "created_at": "Mon, 28 Dec 2020 16:40:41 GMT",
+                            "end_date": "Mon, 01 Mar 2021 00:00:00 GMT",
+                            "event_kind": "상품",
+                            "event_name": "성보의 하루 시리즈",
+                            "event_number": 1,
+                            "event_status": "진행중",
+                            "event_type": "상품(이미지)",
+                            "is_display": "노출",
+                            "product_count": 40,
+                            "start_date": "Mon, 19 Oct 2020 00:00:00 GMT"
+                        }
+                    ]
+                }
+
+            Raises:
+                400, {'message': 'key error', 'errorMessage': 'key_error'}
+                400, {
+                        'message': 'date inputs should be start_date and end_date',
+                        'errorMessage': 'start_date or end_date is missing'
+                    }
+                400, {
+                        'message' : 'event search inputs must be only one',
+                        'errorMessage': 'search value accept only one of name or number'
+                    }
+                400, {'message': 'unable to close database', 'errorMessage': 'unable_to_close_database'}
+                500, {'message': 'internal server error', 'errorMessage': format(e)})
+
+            History:
+                2020-12-28(강두연): 초기 생성
+                2020-12-29(강두연): 1차 수정
+        """
         data = {
             'name': args[0],
             'number': args[1],
