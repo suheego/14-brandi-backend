4d467c31189da30933d926bf5fe314cff6f0190a
devburgundy <gojulie84@gmail.com>
2021-01-01 21:17:38 +0900

    [셀러 상세 페이지] 셀러가 판매하고 있는 셀러 홈 페이지 구현
    [셀러 정보 조회]
    - 셀러 페이지 진입 시 출력되는 셀러 정보 조회 구현
    - 구현 완료

--- service/__init__.py
+++ service/__init__.py
@@ -18,3 +18,6 @@ from .store.destination_service import DestinationService
 from .store.cart_item_service import CartItemService
 from .store.sender_service import SenderService
 from .store.store_order_service import StoreOrderService
+
+
+from .store.seller_shop_service import SellerShopService
