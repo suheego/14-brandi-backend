640303b1e6345b714e73ea3a390ca0c0a66b7dde
ooknimm <ooknimm@gmail.com>
2021-01-01 10:49:46 +0900

    [상품 전체리스트, 카테고리 리스트] 에러 문구 변경

--- view/store/product_list_view.py
+++ view/store/product_list_view.py
@@ -1,5 +1,3 @@
-import json
-
 from flask.views import MethodView
 from flask import jsonify, request
 
@@ -42,8 +40,20 @@ class ProductSearchView(MethodView):
                 connection.close()
 
 
-
 class ProductListView(MethodView):
+    """ Presentation Layer
+
+        Attributes:
+            product_list_service : ProductListService 클래스
+            database             : app.config['DB']에 담겨있는 정보(데이터베이스 관련 정보)
+
+        Author: 김민구
+
+        History:
+            2020-12-29(김민구): 초기 생성
+            2020-12-31(김민구): 에러 문구 변경 / 하나의 이벤트에 대한 배너와 상품들을 반환하는 작업으로 수정
+    """
+
     def __init__(self, services, database):
         self.product_list_service = services.product_list_service
         self.database = database
@@ -59,23 +69,46 @@ class ProductListView(MethodView):
 
             Author: 김민구
 
-            Returns:
-                200, {'message': 'success', 'result': result}                                       : 상품 리스트 조회 성공
+            Returns: 상품 리스트 조회 성공
+                200, {
+                        'message': 'success',
+                        'result': [
+                            {
+                                'id' : 1,
+                                'banner_image' : 'url'
+                            },
+                            {
+                                'image': 'url',
+                                'seller_id': 1,
+                                'seller_name': '둘리',
+                                'product_id': 1,
+                                'product_name': '성보의 하루',
+                                'origin_price': 10000.0,
+                                'discount_rate': 0.1,
+                                'discounted_price': 9000.0,
+                                'sales_count': 30
+                            },
+                        ]
+                    }
 
             Raises:
-                400, {'message': 'invalid_parameter', 'errorMessage': str(e)}                       : 잘못된 요청값
-                400, {'message': 'key_error', 'errorMessage': format(e)}                            : 잘못 입력된 키값
-                404, {'message': 'event_not_exist', 'errorMessage': '이벤트가 더 이상 존재하지 않습니다.'}   : 이벤트가 존재하지 않음
-                500, {'message': 'database_connection_fail', 'errorMessage': 'database_close_fail'} : 커넥션 종료 실패
-                500, {'message': 'database_error', 'errorMessage': format(e)}                       : 데이터베이스 에러
-                500, {'message': 'internal_server_error', 'errorMessage': format(e)})               : 서버 에러
+                400, {'message': 'invalid_parameter', 'error_message': '[데이터]가(이) 유효하지 않습니다.'}  : 잘못된 요청값
+                400, {'message': 'key_error', 'error_message': format(e)}                            : 잘못 입력된 키값
+                404, {'message': 'event_not_exist', 'error_message': '이벤트가 더 이상 존재하지 않습니다.'}   : 이벤트가 존재하지 않음
+                500, {
+                    'message': 'database_connection_fail',
+                    'error_message': '서버에 알 수 없는 에러가 발생했습니다.'
+                    }                                                                                : 커넥션 종료 실패
+                500, {'message': 'database_error', 'error_message': '서버에 알 수 없는 에러가 발생했습니다.'}  : 데이터베이스 에러
+                500, {'message': 'internal_server_error', 'error_message': format(e)})               : 서버 에러
 
             History:
                 2020-12-30(김민구): 초기 생성
-                2020-12-31(김민구): 수정 (이벤트에 해당하는 상품 리스트 반환으로 변경)
+                2020-12-31(김민구): 에러 문구 변경 / 이벤트에 해당하는 상품 리스트 반환으로 수정
 
             Notes:
-                result 리스트 안에 상품정보들과 이벤트 정보가 존재
+                offset을 받아서 한 페이지당 하나의 이벤트만 출력
+                이벤트가 없을 시 빈 리스트 반환
         """
 
         connection = None
